150bcf1a5478af52283086e56ee51aea4b270843
dsethz <dschirethz@gmail.com>
2024-06-04 12:29:33 +0200

    add MSA and feed forward + tests

--- src/torch_model_zoo/models/_tests/test_vit.py
+++ src/torch_model_zoo/models/_tests/test_vit.py
@@ -2,21 +2,36 @@ import numpy as np
 import pytest
 import torch
 
-from torch_model_zoo.models.vit import HeadAttention
+from torch_model_zoo.models.vit import (
+    FeedForward,
+    HeadAttention,
+    MultiHeadAttention,
+)
 
 
 def test_head_init():
     with pytest.raises(AssertionError):
-        _ = HeadAttention(n_embd=0.5, size_head=4, bias=False, scale=None)
+        _ = HeadAttention(
+            n_embd=0.5, size_head=4, bias=False, scale=None, drop=0.2
+        )
 
     with pytest.raises(AssertionError):
-        _ = HeadAttention(n_embd=5, size_head=0.4, bias=False, scale=None)
+        _ = HeadAttention(
+            n_embd=5, size_head=0.4, bias=False, scale=None, drop=0.2
+        )
 
     with pytest.raises(AssertionError):
-        _ = HeadAttention(n_embd=5, size_head=4, bias=2, scale=None)
+        _ = HeadAttention(n_embd=5, size_head=4, bias=2, scale=None, drop=0.2)
 
     with pytest.raises(AssertionError):
-        _ = HeadAttention(n_embd=5, size_head=4, bias=False, scale="abc")
+        _ = HeadAttention(
+            n_embd=5, size_head=4, bias=False, scale="abc", drop=0.2
+        )
+
+    with pytest.raises(AssertionError):
+        _ = HeadAttention(
+            n_embd=5, size_head=4, bias=False, scale=None, drop=False
+        )
 
 
 def test_head():
@@ -31,3 +46,94 @@ def test_head():
     assert isinstance(out, torch.Tensor)
     assert out.dtype == torch.float32
     assert out.shape == (b, n, size_head)
+
+
+def test_mhead_init():
+    with pytest.raises(AssertionError):
+        _ = MultiHeadAttention(
+            n_embd=0.5,
+            n_heads=2,
+            size_head=4,
+            bias=False,
+            scale=None,
+            drop=0.2,
+        )
+
+    with pytest.raises(AssertionError):
+        _ = MultiHeadAttention(
+            n_embd=5,
+            n_heads=0.2,
+            size_head=4,
+            bias=False,
+            scale=None,
+            drop=0.2,
+        )
+
+    with pytest.raises(AssertionError):
+        _ = MultiHeadAttention(
+            n_embd=5,
+            n_heads=2,
+            size_head=0.4,
+            bias=False,
+            scale=None,
+            drop=0.2,
+        )
+
+    with pytest.raises(AssertionError):
+        _ = MultiHeadAttention(
+            n_embd=5, n_heads=2, size_head=4, bias=2, scale=None, drop=0.2
+        )
+
+    with pytest.raises(AssertionError):
+        _ = MultiHeadAttention(
+            n_embd=5, n_heads=2, size_head=4, bias=False, scale="abc", drop=0.2
+        )
+
+    with pytest.raises(AssertionError):
+        _ = MultiHeadAttention(
+            n_embd=5,
+            n_heads=2,
+            size_head=4,
+            bias=False,
+            scale=None,
+            drop=False,
+        )
+
+
+def test_mhead():
+    b, n, c = 3, 10, 8  # tokens size
+    size_head = 4
+    n_heads = 2
+
+    x = np.random.rand(b, n, c)
+    x_t = torch.from_numpy(x).float()
+    heads = MultiHeadAttention(n_embd=c, n_heads=n_heads, size_head=size_head)
+    out = heads(x_t)
+
+    assert isinstance(out, torch.Tensor)
+    assert out.dtype == torch.float32
+    assert out.shape == (b, n, c)
+
+
+def test_ff_init():
+    with pytest.raises(AssertionError):
+        _ = FeedForward(n_embd=0.5, bias=False, drop=None)
+
+    with pytest.raises(AssertionError):
+        _ = FeedForward(n_embd=5, bias=2, drop=None)
+
+    with pytest.raises(AssertionError):
+        _ = FeedForward(n_embd=5, bias=False, drop=True)
+
+
+def test_ff():
+    b, n, c = 3, 10, 8  # input shape
+
+    x = np.random.rand(b, n, c)
+    x_t = torch.from_numpy(x).float()
+    ff = FeedForward(n_embd=c, bias=False, drop=0.2)
+    out = ff(x_t)
+
+    assert isinstance(out, torch.Tensor)
+    assert out.dtype == torch.float32
+    assert out.shape == (b, n, c)
--- src/torch_model_zoo/models/vit.py
+++ src/torch_model_zoo/models/vit.py
@@ -6,9 +6,9 @@
 # PyTorch Version:      2.3.0                                                                                         #
 # PyTorch Lightning Version: 1.5.9                                                                                    #
 #######################################################################################################################
-
 from typing import Optional
 
+import torch
 from torch import nn
 
 
@@ -23,7 +23,7 @@ class HeadAttention(nn.Module):
         size_head: int,
         bias: bool = False,
         scale: Optional[float] = None,
-        attn_drop: float = 0.2,
+        drop: float = 0.2,
     ):
         """
         Constructor.
@@ -38,8 +38,8 @@ class HeadAttention(nn.Module):
             True if bias should be used for linear layers of Q, K, and V.
         scale: Optional[float]
             Scaling factor of self-attention. If None (default) use 1/sqrt(size_head).
-        attn_drop: float
-            Dropout rate of attention layer.
+        drop: float
+            Dropout rate.
 
         Returns
         -------
@@ -60,8 +60,8 @@ class HeadAttention(nn.Module):
             scale, float
         ), f"scale must be float, but is {type(scale)}."
         assert isinstance(
-            attn_drop, float
-        ), f"attn_drop must be float, but is {type(attn_drop)}."
+            drop, float
+        ), f"drop must be float, but is {type(drop)}."
 
         self.n_embd = n_embd
         self.size_head = size_head
@@ -70,7 +70,7 @@ class HeadAttention(nn.Module):
         self.key = nn.Linear(n_embd, size_head, bias=bias)
         self.query = nn.Linear(n_embd, size_head, bias=bias)
         self.value = nn.Linear(n_embd, size_head, bias=bias)
-        self.attn_drop = nn.Dropout(attn_drop)
+        self.drop = nn.Dropout(drop)
 
     def forward(self, x):
         B, N, C = x.shape
@@ -83,6 +83,139 @@ class HeadAttention(nn.Module):
         # calculate attention
         attn = (q @ k.transpose(-2, -1)) * self.scale
         attn = attn.softmax(dim=-1)
-        attn = self.attn_drop(attn)
+        attn = self.drop(attn)
 
         return attn @ v
+
+
+class MultiHeadAttention(nn.Module):
+    """
+    This class defines an MSA block.
+    """
+
+    def __init__(
+        self,
+        n_embd: int,
+        n_heads: int,
+        size_head: int,
+        bias: bool = False,
+        scale: Optional[float] = None,
+        drop: float = 0.2,
+    ):
+        """
+        Constructor.
+
+        Parameters
+        ----------
+        n_embd: int
+            Input channels of single head.
+        n_heads: int
+            Number of attention heads.
+        size_head: int
+            Output channels of single head.
+        bias: bool
+            True if bias should be used for linear layers of Q, K, and V.
+        scale: Optional[float]
+            Scaling factor of self-attention. If None (default) use 1/sqrt(size_head).
+        drop: float
+            Dropout rate.
+
+        Returns
+        -------
+        None
+        """
+        super().__init__()
+
+        assert isinstance(
+            n_embd, int
+        ), f"n_embd must be int, but is {type(n_embd)}."
+        assert isinstance(
+            n_heads, int
+        ), f"n_heads must be int, but is {type(n_heads)}."
+        assert isinstance(
+            size_head, int
+        ), f"size_head must be int, but is {type(size_head)}."
+        assert isinstance(
+            bias, bool
+        ), f"bias must be bool, but is {type(bias)}."
+        assert scale is None or isinstance(
+            scale, float
+        ), f"scale must be float, but is {type(scale)}."
+        assert isinstance(
+            drop, float
+        ), f"drop must be float, but is {type(drop)}."
+
+        self.n_heads = n_heads
+        self.size_head = size_head
+
+        self.heads = nn.ModuleList(
+            [
+                HeadAttention(
+                    n_embd=n_embd,
+                    size_head=size_head,
+                    bias=bias,
+                    scale=scale,
+                    drop=drop,
+                )
+                for _ in range(n_heads)
+            ]
+        )
+        self.proj = nn.Linear(size_head * n_heads, n_embd, bias=bias)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        out = torch.cat([head(x) for head in self.heads], dim=-1)
+        out = self.proj(out)
+        out = self.drop(out)
+
+        return out
+
+
+class FeedForward(nn.Module):
+    """
+    This class defines an feed forward block.
+    """
+
+    def __init__(
+        self,
+        n_embd: int,
+        bias: bool = False,
+        drop: float = 0.2,
+    ):
+        """
+        Constructor.
+
+        Parameters
+        ----------
+        n_embd: int
+            Input channels of single head.
+        bias: bool
+            True if bias should be used for linear layers of Q, K, and V.
+        drop: float
+            Dropout rate.
+
+        Returns
+        -------
+        None
+        """
+        super().__init__()
+
+        assert isinstance(
+            n_embd, int
+        ), f"n_embd must be int, but is {type(n_embd)}."
+        assert isinstance(
+            bias, bool
+        ), f"bias must be bool, but is {type(bias)}."
+        assert isinstance(
+            drop, float
+        ), f"drop must be float, but is {type(drop)}."
+
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd * 4, bias=bias),
+            nn.GELU(),
+            nn.Linear(n_embd * 4, n_embd, bias=bias),
+            nn.Dropout(drop),
+        )
+
+    def forward(self, x):
+        return self.net(x)
